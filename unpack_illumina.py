468a2c1585206a6f4e7fffc2f09f2942198deaf1
Ben Lorentz <blorentz@luc.edu>
2019-12-06 10:20:52 -0600

    Bare bones idea

---
+++ unpack_illumina.py
@@ -0,0 +1,18 @@
+# A python script to unpack fastq files from illumina basespace and place in a folder 
+
+def create_index_of_fastq():
+    index = []
+    print("created")
+    print("index saved to file")
+    return index
+
+def move_fastq_to_output_dir(index):
+    print("moved")
+
+def unzip_fastqgz():
+    print("unzipped")
+
+if __name__ == "main":
+    index = create_index_of_fastq()
+    move_fastq_to_output_dir(index)
+    unzip_fastqgz()
