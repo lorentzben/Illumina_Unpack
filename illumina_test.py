bb7ba62206b2d51bc5d2b645fd64835f2df3b6ac
Ben Lorentz <blorentz@luc.edu>
2020-01-09 11:48:56 -0600

    oopS

--- illumina_test.py
+++ illumina_test.py
@@ -18,9 +18,10 @@ for item in list_of_fastq:
     filesize = item.stat().st_size
     discovered_fastqs.append(tuple([tuple([filename, filesize]), item]))
 
-duplicates = [] 
+duplicates = []
+final_list = []
 for item in discovered_fastqs:
-        
+
     filename = item[0][0]
     if filename not in final_list:
         final_list.append(filename)
@@ -28,17 +29,16 @@ for item in discovered_fastqs:
         duplicates.append(filename)
 
 item_to_remove = []
-#looks over items that were duplicated and finds the object associated with the filename and creates a list of objects
+# looks over items that were duplicated and finds the object associated with the filename and creates a list of objects
 for item in duplicates:
     for source in discovered_fastqs:
         if item == source[0][0]:
             item_to_remove.append(source)
-#sorts list of objects based on filesize
-sorted(item_to_remove, key=lambda x:x[0][1])
-#removes objects from filelist until no more duplicates remain, will be removing the small files first
-while list_of_duplicates is not []:
+# sorts list of objects based on filesize
+sorted(item_to_remove, key=lambda x: x[0][1])
+# removes objects from filelist until no more duplicates remain, will be removing the small files first
+while duplicates is not []:
     for thing in item_to_remove:
         discovered_fastqs.remove(thing)
         duplicates.remove(thing[0][0])
-print(str(list_of_fastqs))
-    
+print(str(discovered_fastqs))
